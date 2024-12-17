e308f845bf2af049bcf775f538b3faa1f3aa4fae
rabi-siddique <rabisiddique27@gmail.com>
2024-12-17 16:34:51 +0500

    ci: get active vaults

---
+++ .github/scripts/changePrice.mjs
@@ -0,0 +1,36 @@
+import { execa } from 'execa';
+import assert from 'assert';
+
+const agops = '/usr/src/agoric-sdk/packages/agoric-cli/bin/agops';
+
+const amount = process.env.amount;
+const containerName = process.env.containerName;
+const agoricNet = process.env.agoricNet;
+
+if (!amount || !containerName || !agoricNet) {
+  console.error('Error: Missing one or more required parameters:');
+  if (!amount) console.error('Missing amount');
+  if (!containerName) console.error('Missing containerName');
+  if (!agoricNet) console.error('Missing agoricNet');
+  process.exit(1);
+}
+
+const setAtomPrice = async (amount, containerName, agoricNet) => {
+  try {
+    console.log(`Initiating price change to ${amount} for ATOM...`);
+
+    const env = {
+      AGORIC_NET: agoricNet,
+    };
+
+    const command = `${agops} oracle setPrice --keys gov1,gov2 --pair ATOM.USD --price ${amount} --keyring-backend=test`;
+    const { stdout, stderr } = await execa('docker', ['exec', containerName, command], { env, shell: true });
+
+    assert.strictEqual(stderr, '', `Expected stderr to be empty, but received: '${stderr}'`);
+    console.log('Standard output:', stdout);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+setAtomPrice(amount, containerName, agoricNet);
